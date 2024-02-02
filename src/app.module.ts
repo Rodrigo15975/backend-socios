c68fc372fdd97dcde4656646afa3abbd93413798
BraunRumpler <javierluis15975@hotmail.com>
2024-02-02 14:23:22 -0500

    template-nest-v-0.3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { SharedPostgresqlModule } from './shared/postgresql/Sharedpostgresql.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SharedPostgresqlModule, SharedPostgresqlModule, AuthModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [],
   providers: [],
 })
